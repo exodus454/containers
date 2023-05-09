8b59eac4260fa31599e5054d5385aab30f35197e
Dan Stearns <exodus454@gmail.com>
2023-05-09 17:53:15 -0400

    ffmpeg-wrap

--- apps/jellyfin/entrypoint.sh
+++ apps/jellyfin/entrypoint.sh
@@ -6,7 +6,7 @@ test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 #shellcheck disable=SC2086
 exec \
     /usr/bin/jellyfin \
-        --ffmpeg="/usr/lib/jellyfin-ffmpeg/ffmpeg" \
+        --ffmpeg="/usr/lib/jellyfin-ffmpeg/ffmpeg.wrap" \
         --webdir="/usr/share/jellyfin/web" \
         --datadir="/config" \
         --cachedir="/config/cache" \
